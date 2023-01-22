a8e242fe5645e36a01e0ccd2c3c08560155c6134
UserBRAvo99 <108820828+UserBRAvo99@users.noreply.github.com>
2023-01-22 20:32:43 +0200

    task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -7,15 +7,15 @@ const getGallatyEl = document.querySelector(".gallery");
 const makeGallary = galleryItems
   .map((element) => {
     return `<div class="gallery__item">
-    <a class="gallery__link" href="${element.original}">
-    <img
-        class="gallery__image"
-        src="${element.preview}"
-        data-source="${element.original}"
-        alt="${element.description}"
-    />
-    </a>
-</div>`;
+        <a class="gallery__link" href="${element.original}">
+        <img
+            class="gallery__image"
+            src="${element.preview}"
+            data-source="${element.original}"
+            alt="${element.description}"
+        />
+        </a>
+    </div>`;
   })
   .join("");
 getGallatyEl.insertAdjacentHTML("beforebegin", makeGallary);
