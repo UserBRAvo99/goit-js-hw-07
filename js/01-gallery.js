b1bf8ea0b7e68b30248ac970b783eb0f4140bbc2
UserBRAvo99 <108820828+UserBRAvo99@users.noreply.github.com>
2023-01-22 19:32:48 +0200

    task1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,21 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+const getGallatyEl = document.querySelector(".gallery");
+
+const makeGallary = galleryItems
+  .map((element) => {
+    return `<div class="gallery__item">
+    <a class="gallery__link" href="${element.original}">
+    <img
+        class="gallery__image"
+        src="${element.preview}"
+        data-source="${element.original}"
+        alt="${element.description}"
+    />
+    </a>
+</div>`;
+  })
+  .join("");
+getGallatyEl.insertAdjacentHTML("beforebegin", makeGallary);
