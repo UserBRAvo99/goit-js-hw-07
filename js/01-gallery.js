f28bb9b3126594f21fc41915e4eb672f2950e220
UserBRAvo99 <108820828+UserBRAvo99@users.noreply.github.com>
2023-01-23 19:14:38 +0200

    task71

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,21 +1,45 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
 const getGallatyEl = document.querySelector(".gallery");
 
 const makeGallary = galleryItems
   .map((element) => {
     return `<div class="gallery__item">
-        <a class="gallery__link" href="${element.original}">
-        <img
-            class="gallery__image"
-            src="${element.preview}"
-            data-source="${element.original}"
-            alt="${element.description}"
-        />
-        </a>
-    </div>`;
+  <a class="gallery__link" href="${element.original}">
+    <img
+      class="gallery__image"
+      src="${element.preview}"
+      data-source="${element.original}"
+      alt="${element.description}"
+    />
+  </a>
+</div>`;
   })
   .join("");
-getGallatyEl.insertAdjacentHTML("beforebegin", makeGallary);
+
+getGallatyEl.insertAdjacentHTML("beforeend", makeGallary);
+
+getGallatyEl.addEventListener("click", (event) => {
+  event.preventDefault();
+  if (!event.target.closest(".gallery__image")) {
+    return;
+  }
+  const instance = basicLightbox.create(`
+      <img src="${event.target.dataset.source}" width="800" height="600">
+  `);
+  instance.show();
+  const visible = basicLightbox.visible();
+  //перевіряємо чи модальне вікно відкрите
+  if (visible) {
+    //додаємо слухача на кнопку
+    document.addEventListener("keydown", (event) => {
+      console.dir(event.key);
+      if (event.key === "Escape") {
+        //закриваємо вікно при натисканні "Escape"
+        instance.close();
+        document.removeEventListener("keydown", event);
+      }
+    });
+  }
+});
